ca63742f69b0afd647268457662e4299c20a9898
Logan Willis <loganw0315@gmail.com>
2021-12-22 14:13:09 -0600

    first commit

---
+++ src/components/Modal.js
@@ -0,0 +1,16 @@
+import './Modal.css'
+
+export default function Modal({ children, handleClose }) {
+  return (
+    <div className="modal-backdrop">
+      <div className="modal" style={{
+        border: '4px solid', 
+        borderColor:'#ff4500',
+        textAlign:'center'
+        }}>
+        {children}
+        <button onClick={handleClose}>Close</button>
+      </div>
+    </div>
+  )
+}

98e467e8f506b138f9d5ace85dc1d3a425983ec3
Logan Willis <loganw0315@gmail.com>
2021-12-23 10:58:17 -0600

    added form submission

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,15 +1,53 @@
+import { useRef, useState } from "react";
 import "./NewEventForm.css"
 
-const NewEventForm = () => {
+const NewEventForm = ({addEvent}) => {
+
+    // const [title, setTitle] = useState('')
+    // const [date, setDate] = useState('')
+    const title = useRef()
+    const date = useRef()
+
+
+    const resetForm = () => {
+        // setTitle('')
+        // setDate('')
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+      const event = {
+          title: title,
+          date: date,
+          id: Math.floor(Math.random()*10000)
+      }
+      addEvent(event);
+      console.log(event);
+      resetForm()
+    }
+
+    
+
     return (
-        <form className="new-event-form">
+        <form className="new-event-form" onSubmit={handleSubmit}>
             <label>
                 <span>Event Title:</span>
-                <input type="text" />
+                <input 
+                    type="text" 
+                    ref={title}
+                    // onChange={(e)=> setTitle(e.target.value)}
+                    // value={title}
+                />
             </label>
             <label>
                 <span>Event Date:</span>
-                <input type="date" />
+                <input 
+                    type="date" 
+                    ref={date}
+                    // onChange={(e)=> setDate(e.target.value)}
+                    // value={date}
+                />
             </label>
             <button>Submit</button>
         </form>
